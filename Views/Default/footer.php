a3531ca243f15e8e578cc48c7d701f88f0945fe8
Lynneth Pereira <lynnethpereira12@gamail.com>
2023-06-07 17:29:31 -0400

    Styles

---
+++ Views/Default/footer.php
@@ -0,0 +1,9 @@
+<footer class="border-top footer text-muted">
+    <div align="center">
+        <p>&copy; Lynneth Pereira</p>
+    </div>
+</footer>
+<script src="<?php echo URL.RS ?>js/jquery-3.7.0.min.js"></script>
+<script src="<?php echo URL.RS ?>js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ Views/Default/head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo URL.RS ?>css/bootstrap.css">
+    <title>Sales System</title>
+</head>
+<body>
