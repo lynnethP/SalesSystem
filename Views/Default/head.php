40ddeb8ed2e4e785a83b665415bb1a12719944c1
Lynneth Pereira <lynnethpereira12@gamail.com>
2023-06-08 17:51:35 -0400

    Styles y Navbar

--- Views/Default/head.php
+++ Views/Default/head.php
@@ -1,10 +1,51 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo URL.RS ?>css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo URL . RS ?>css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo URL . RS ?>css/style.css">
     <title>Sales System</title>
 </head>
+
 <body>
+
+
+    <header>
+        <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
+            <div class="container">
+                <a href="" class="navbar-brand">
+                    <img src="<?php echo URL . RS ?>img/icons/IconSystem.png" class="mx-auto w-25 imgLogo" alt="Logo">
+                    SalesSystem</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo" aria-controls="navbarTogglerDemo" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse" id="navbarTogglerDemo">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="" class="nav-link text-dark" title="Manage">Hello</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link text-dark" title="Manage">Logout</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link text-dark" title="Manage">Register</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link text-dark" title="Manage">Login</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav flex-grow-1">
+                        <li class="nav-item">
+                            <a href="" class="nav-link text-dark">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link text-dark">Users</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
